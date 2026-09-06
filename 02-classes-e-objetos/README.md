f553ef1df44b4fd741832b62514543633463eb06
Daniel Silveira <daniell_msn@hotmail.com>
2026-09-06 10:54:46 -0300

    New section of the project Classes and objects

---
+++ 02-classes-e-objetos/README.md
@@ -0,0 +1,8 @@
+# Classes and Objects
+
+This module explores the fundamental concepts of classes and objects
+using practical examples in C#.
+
+The goal is to understand how classes define the structure and behavior
+of objects, and how multiple objects can be created from the same class
+while maintaining independent states.
---
+++ 02-classes-e-objetos/program.cs
@@ -0,0 +1,50 @@
+﻿class ContaBancaria
+{
+    public string Titular;
+    public double Saldo;
+    
+    public ContaBancaria(string titular, double saldo)
+    {
+        Titular = titular;
+        Saldo = saldo;
+    }
+    public void Sacar(double valor)
+    {
+    Saldo -= valor;
+    }
+    public void Depositar(double valor)
+    {
+        Saldo += valor;
+    }
+    public double ConsultarSaldo()
+    {
+        return Saldo;
+    }
+}   
+class Program
+{
+    static void Main()
+    {
+        ContaBancaria conta1 = new ContaBancaria("Daniel", 2000);
+        conta1.Sacar(500);
+        conta1.Depositar(1000);
+        ContaBancaria conta2 = new ContaBancaria("Maria", 500);
+        ContaBancaria conta3 = new ContaBancaria("João", 3000);
+
+        Console.WriteLine("Conta 1:");
+        Console.WriteLine("Titular: " + conta1.Titular);
+        Console.WriteLine("Saldo: " + conta1.ConsultarSaldo());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Conta 2:");
+        Console.WriteLine("Titular: " + conta2.Titular);
+        Console.WriteLine("Saldo: " + conta2.Saldo);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Conta 3:");
+        Console.WriteLine("Titular: " + conta3.Titular);
+        Console.WriteLine("Saldo: " + conta3.Saldo);
+    }
+}
